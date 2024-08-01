4a032f40a6f56d97fb1db351fd8f53bebd92dd22
Riccardo <ric.cavaleri@gmail.com>
2024-08-01 19:00:28 +0200

    Esercizio

---
+++ src/main/java/com/example/esercizio_develhope/EsercizioDevelhopeApplication.java
@@ -0,0 +1,13 @@
+package com.example.esercizio_develhope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioDevelhopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioDevelhopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/esercizio_develhope/EsercizioDevelhopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esercizio_develhope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioDevelhopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
